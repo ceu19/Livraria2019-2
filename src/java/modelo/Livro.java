2a4b9fb11f85551572efe42263da6919d07487bd
Aluno <Aluno@lab2pc2>
2019-08-16 11:41:12 -0300

    Commit inicial

---
+++ src/java/modelo/Livro.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Aluno
+ */
+@Entity
+public class Livro implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    public String titulo;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    public Date datalanc;
+    public Number pag;
+    public String sinopse;
+    public String idioma;
+    public String fotos;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Date getDatalanc() {
+        return datalanc;
+    }
+
+    public void setDatalanc(Date datalanc) {
+        this.datalanc = datalanc;
+    }
+
+    public Number getPag() {
+        return pag;
+    }
+
+    public void setPag(Number pag) {
+        this.pag = pag;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public String getFotos() {
+        return fotos;
+    }
+
+    public void setFotos(String fotos) {
+        this.fotos = fotos;
+    }
+    
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Livro)) {
+            return false;
+        }
+        Livro other = (Livro) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Livro[ id=" + id + " ]";
+    }
+    
+}
---
+++ web/WEB-INF/livro.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : livro
+    Created on : 16/08/2019, 09:08:20
+    Author     : Aluno
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
